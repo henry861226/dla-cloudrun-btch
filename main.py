ddb5ab7a7468cf02a22a223f583899c31df668db
henry <henry28921094@gmail.com>
2024-12-20 13:26:38 +0800

    test sync data

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ processed_events = set()
 @app.route("/syncData", methods=["POST"])
 def handle_request():
     try:
-        # event_data = request.get_json()
-        # event_id = event_data.get('id')
-        # # 檢查事件是否已處理
-        # if event_id in processed_events:
-        #     logging.info(f"Event {event_id} 已處理過，跳過。")
-        #     return "Event already processed.", 200
-        # # 檢查GCS批次檔案是否準備好
-        # logging.info("Received event:%s", event_data)
-        # logging.info("event id: %s", event_id)
-        # logging.info("object createtime: %s", event_data.get('timeCreated'))
+        event_data = request.get_json()
+        event_id = event_data.get('id')
+        # 檢查事件是否已處理
+        if event_id in processed_events:
+            logging.info(f"Event {event_id} 已處理過，跳過。")
+            return "Event already processed.", 200
+        # 檢查GCS批次檔案是否準備好
+        logging.info("Received event:%s", event_data)
+        logging.info("event id: %s", event_id)
+        logging.info("object createtime: %s", event_data.get('timeCreated'))
         if not check_gcs_file_ready(os.getenv('GCS_BUCKET'), os.getenv('DAILY_FILE')):
             return f"GCS 檔案 {os.getenv('DAILY_FILE')} 尚未準備就緒，請稍後再試。", 200
-        sync_data_main()
-        return "Success syncing batch data.", 200
+        else:
+            #sync_data_main()
+            return "Success syncing batch data.", 200
     except Exception as e:
             logging.error(f"syncdata request發生錯誤: {e}")
             raise
